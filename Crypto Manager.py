9c06b528f95c2531e4c2c6e7c8bd1f2725ffc575
Zack Beauchamp <zack.beauchamp@durham.ac.uk>
2018-12-17 18:50:17 +0000

    Added sample database file & changed all attributes and methods in classes to private

--- Crypto Manager.py
+++ Crypto Manager.py
@@ -41,9 +41,9 @@ class CryptoWindow:
 
         #self.__view_table_button = tk.Button(self.__button_frame, text = "View Table", height = 2, width = 15, command = lambda:self.__view_table())
         #self.__view_table_button.grid(row = 0, column = 0, padx = 3, pady = 3)
-        self.__valuation_button = tk.Button(self.__button_frame, text = "Valuation", height = 2, width = 15, command = lambda:self.get_valuation())
+        self.__valuation_button = tk.Button(self.__button_frame, text = "Valuation", height = 2, width = 15, command = lambda:self.__get_valuation())
         self.__valuation_button.grid(row = 0, column = 1, padx = 3, pady = 3)
-        self.__edit_invested_button = tk.Button(self.__button_frame, text = "Edit Invested", height = 2, width = 15, command = lambda:self.edit_invested())
+        self.__edit_invested_button = tk.Button(self.__button_frame, text = "Edit Invested", height = 2, width = 15, command = lambda:self.__edit_invested())
         self.__edit_invested_button.grid(row = 0, column = 2, padx = 3, pady = 3)
 
     def __close(self):
@@ -70,18 +70,18 @@ class CryptoWindow:
     ###TABLE FUNCTIONS###
 
     def __view_table(self):
-        self.columns = ("id", "symbol", "holdings")
-        self.tree = ttk.Treeview(self.__widget_frame, columns=self.columns[1:])
+        self.__columns = ("id", "symbol", "holdings")
+        self.__tree = ttk.Treeview(self.__widget_frame, columns=self.__columns[1:])
         i = 0
-        for column in self.columns:
+        for column in self.__columns:
             #print("C: {0}".format(column))
-            self.tree.heading("#{0}".format(i), text = column)
-            self.tree.column("#{0}".format(i), stretch=tk.NO)
+            self.__tree.heading("#{0}".format(i), text = column)
+            self.__tree.column("#{0}".format(i), stretch=tk.NO)
             i += 1
-        self.tree.grid(row=2, columnspan=3, sticky="nsew")
-        self.treeview = self.tree
-        self.tree.bind("<Double-1>", self.select_item)
-        self.selected = None
+        self.__tree.grid(row=2, columnspan=3, sticky="nsew")
+        self.__treeview = self.__tree
+        self.__tree.bind("<Double-1>", self.__select_item)
+        self.__selected = None
         
         sql = """SELECT * FROM crypto_table"""
         self.__cursor.execute(sql)
@@ -92,88 +92,88 @@ class CryptoWindow:
                 temp.append(each[i])
             values = tuple(temp)
             if values[1] != 0 or 1:
-                self.treeview.insert('', 'end', text=each[0], values=values)
+                self.__treeview.insert('', 'end', text=each[0], values=values)
 
-        self.newButton = tk.Button(self.__widget_frame, text = "New Record", height = 3, width = 25, font = ("TkDefaultFont", 10), command = lambda:self.new_record())
-        self.editButton = tk.Button(self.__widget_frame, text = "Edit Record", height = 3, width = 25, font = ("TkDefaultFont", 10), command = lambda:self.edit_record())
-        self.removeButton = tk.Button(self.__widget_frame, text = "Remove Record", height = 3, width = 25, font = ("TkDefaultFont", 10), command = lambda:self.remove_record())
-        self.selectedLabel = tk.Label(self.__widget_frame, text = "")
+        self.__newButton = tk.Button(self.__widget_frame, text = "New Record", height = 3, width = 25, font = ("TkDefaultFont", 10), command = lambda:self.__new_record())
+        self.__editButton = tk.Button(self.__widget_frame, text = "Edit Record", height = 3, width = 25, font = ("TkDefaultFont", 10), command = lambda:self.__edit_record())
+        self.__removeButton = tk.Button(self.__widget_frame, text = "Remove Record", height = 3, width = 25, font = ("TkDefaultFont", 10), command = lambda:self.__remove_record())
+        self.__selectedLabel = tk.Label(self.__widget_frame, text = "")
         
-        self.newButton.grid(row = 0, column = 0, pady = 1)
-        self.editButton.grid(row = 0, column = 1, pady = 1)
-        self.removeButton.grid(row = 0, column = 2, pady = 1)
-        self.selectedLabel.grid(row = 1, column = 0, sticky = "w", pady = 2)
+        self.__newButton.grid(row = 0, column = 0, pady = 1)
+        self.__editButton.grid(row = 0, column = 1, pady = 1)
+        self.__removeButton.grid(row = 0, column = 2, pady = 1)
+        self.__selectedLabel.grid(row = 1, column = 0, sticky = "w", pady = 2)
 
-    def select_item(self, e):
-        selected = self.tree.focus()
+    def __select_item(self, e):
+        selected = self.__tree.focus()
         if len(selected) > 0:
-            self.selected = self.tree.item(selected, "text")
-            self.selectedLabel.config(text= "   Selected id: {0}".format(self.selected))
-        #print(self.selected)
+            self.__selected = self.__tree.item(selected, "text")
+            self.__selectedLabel.config(text= "   Selected id: {0}".format(self.__selected))
+        #print(self.__selected)
         
-    def new_record(self):
-        self.task = "new"
-        self.record_view("New Record")
+    def __new_record(self):
+        self.__task = "new"
+        self.__record_view("New Record")
 
-    def edit_record(self): #Fills record view for edit
-        if self.selected is not None:
-            self.task = "edit"
-            self.__cursor.execute("""SELECT * FROM crypto_table WHERE id='"""+str(self.selected)+"""'""")
+    def __edit_record(self): #Fills record view for edit
+        if self.__selected is not None:
+            self.__task = "edit"
+            self.__cursor.execute("""SELECT * FROM crypto_table WHERE id='"""+str(self.__selected)+"""'""")
             data = self.__cursor.fetchone()
-            self.record_view("Record: {0}".format(self.selected))
+            self.__record_view("Record: {0}".format(self.__selected))
             #print(data)
             for i in range(0, len(data)):
-                    self.inputs[i].insert(0, data[i])
+                    self.__inputs[i].insert(0, data[i])
 
-    def remove_record(self):
-        self.__cursor.execute("""DELETE FROM crypto_table WHERE id='"""+str(self.selected)+"""'""")
+    def __remove_record(self):
+        self.__cursor.execute("""DELETE FROM crypto_table WHERE id='"""+str(self.__selected)+"""'""")
         self.__db.commit()
         self.__view_table()
 
-    def record_view(self, title): #Record view for edit and new
-        self.recordView = tk.Tk()
-        self.recordView.title(title)
-        self.recordView.resizable(False, False)
+    def __record_view(self, title): #Record view for edit and new
+        self.__recordView = tk.Tk()
+        self.__recordView.title(title)
+        self.__recordView.resizable(False, False)
 
-        self.labelFrame = tk.Frame(self.recordView)
-        self.inputFrame = tk.Frame(self.recordView)
+        self.__labelFrame = tk.Frame(self.__recordView)
+        self.__inputFrame = tk.Frame(self.__recordView)
         
-        self.labelFrame.grid(row = 0, column = 0)
-        self.inputFrame.grid(row = 0, column = 1)
+        self.__labelFrame.grid(row = 0, column = 0)
+        self.__inputFrame.grid(row = 0, column = 1)
 
         labels = []
-        self.inputs = []
+        self.__inputs = []
         i = 0
-        for column in self.columns:
-            labels.append(tk.Label(self.labelFrame, text = "{0}:".format(column)))
-            self.inputs.append(tk.Entry(self.inputFrame))
+        for column in self.__columns:
+            labels.append(tk.Label(self.__labelFrame, text = "{0}:".format(column)))
+            self.__inputs.append(tk.Entry(self.__inputFrame))
 
             labels[i].grid(row = i, column = 0, sticky = "e", pady = 1)
-            self.inputs[i].grid(row = i, column = 1, sticky = "w", pady = 1)
+            self.__inputs[i].grid(row = i, column = 1, sticky = "w", pady = 1)
 
             i += 1
 
-        self.cancel = tk.Button(self.recordView, text = "Cancel", height = 2, width = 20, command = lambda:self.record_view_close(True))
-        self.cancel.grid(row = 1, column = 0)
-        self.confirm = tk.Button(self.recordView, text = "Confirm", height = 2, width = 20, command = lambda:self.record_view_close())
-        self.confirm.grid(row = 1, column = 1)
+        self.__cancel = tk.Button(self.__recordView, text = "Cancel", height = 2, width = 20, command = lambda:self.__record_view_close(True))
+        self.__cancel.grid(row = 1, column = 0)
+        self.__confirm = tk.Button(self.__recordView, text = "Confirm", height = 2, width = 20, command = lambda:self.__record_view_close())
+        self.__confirm.grid(row = 1, column = 1)
 
     def __unselect(self):
-        self.selectedLabel.config(text= " "*40)
-        self.selected = None
+        self.__selectedLabel.config(text= " "*40)
+        self.__selected = None
         
-    def record_view_close(self, cancel = False): #Closes record view
+    def __record_view_close(self, cancel = False): #Closes record view
         if cancel:
-            self.recordView.destroy()
+            self.__recordView.destroy()
         else:
             values = []
-            for i in range(0, len(self.inputs)):
+            for i in range(0, len(self.__inputs)):
                 if i == 0:
-                    newVal = self.inputs[i].get().lower()
+                    newVal = self.__inputs[i].get().lower()
                 elif i == 1:
-                    newVal = self.inputs[i].get().upper()
+                    newVal = self.__inputs[i].get().upper()
                 else:
-                    newVal = self.inputs[i].get()
+                    newVal = self.__inputs[i].get()
                 values.append(newVal)
             #print(values)
             flag = False #Flags if any values are empty
@@ -183,17 +183,17 @@ class CryptoWindow:
             if flag:
                 print("Error: Empty value(s)")
             else:
-                if self.task == "edit":
+                if self.__task == "edit":
                     sql = """UPDATE crypto_table SET """
                     for i in range(0, len(values)):
-                        sql += self.columns[i] + """ = '"""+str(values[i])+"""', """
-                    sql = sql[:-2] + """ WHERE id='"""+str(self.selected)+"""'"""
-                elif self.task == "new":
+                        sql += self.__columns[i] + """ = '"""+str(values[i])+"""', """
+                    sql = sql[:-2] + """ WHERE id='"""+str(self.__selected)+"""'"""
+                elif self.__task == "new":
                     sql = """INSERT INTO crypto_table ("""
-                    for i in range(0, len(self.columns)):
-                        sql += self.columns[i] +""", """
+                    for i in range(0, len(self.__columns)):
+                        sql += self.__columns[i] +""", """
                     sql = sql[:-2] + """) VALUES ('"""
-                    for i in range(0, len(self.columns)):
+                    for i in range(0, len(self.__columns)):
                         sql += str(values[i])+"""', '"""
                     sql = sql[:-4] + """')"""
                 #print(sql)
@@ -202,13 +202,13 @@ class CryptoWindow:
                 self.__view_table()
                 
                 self._master.deiconify()
-                self.recordView.destroy()
-        if self.task == "new" or not cancel:
+                self.__recordView.destroy()
+        if self.__task == "new" or not cancel:
             self.__unselect()
 
         ###TABLE FUNCTIONS END###
 
-    def get_valuation(self, currency = "gbp"):
+    def __get_valuation(self, currency = "gbp"):
         self.__cursor.execute("""SELECT id, symbol, holdings FROM crypto_table""")
         results = self.__cursor.fetchall()
         self.__cursor.execute("""SELECT invested FROM invested_table""")
@@ -224,40 +224,40 @@ class CryptoWindow:
                 valueR = round_(value)
                 total += value
                 totalR += valueR
-                print("{0:>7} {1:^5} = £{2:<7.2f} >>  £{3:.2f}".format(crypto[2], crypto[1], value, valueR))
+                print("{0:>8} {1:^5} = £{2:<8.2f} >>  £{3:.2f}".format(crypto[2], crypto[1], value, valueR))
             
         print("-----\nTotal Blockfolio Value: £{0:<7.2f} >>  £{1:.2f}".format(total, totalR))
         print("Total Investment Made:  £{0}, P/L: {1}£{2:.2f} ({3:.1f}%)".format(invested, isPos(total - invested), abs(total - invested), (total - invested)*100 / invested))
         print("@ {} on {}\n-----".format(getTime(1)[1], getTime(0)))
 
-    def edit_invested(self):
-        self.investedView = tk.Tk()
-        self.investedView.title("Edit Invested")
-        self.investedView.resizable(False, False)
+    def __edit_invested(self):
+        self.__investedView = tk.Tk()
+        self.__investedView.title("Edit Invested")
+        self.__investedView.resizable(False, False)
 
-        current_label = tk.Label(self.investedView, text = "Current:")
+        current_label = tk.Label(self.__investedView, text = "Current:")
         current_label.grid(row = 0, column = 0)
-        current_amount = tk.Label(self.investedView, text = self.get_invested())
+        current_amount = tk.Label(self.__investedView, text = self.__get_invested())
         current_amount.grid(row = 0, column = 1)
-        invested_label = tk.Label(self.investedView, text = "Invested:")
+        invested_label = tk.Label(self.__investedView, text = "Invested:")
         invested_label.grid(row = 1, column = 0)
-        self.invested_entry = tk.Entry(self.investedView)
-        self.invested_entry.grid(row = 1, column = 1)
+        self.__invested_entry = tk.Entry(self.__investedView)
+        self.__invested_entry.grid(row = 1, column = 1)
 
-        i_cancel = tk.Button(self.investedView, text = "Cancel", height = 2, width = 20, command = lambda:self.invested_view_close(True))
+        i_cancel = tk.Button(self.__investedView, text = "Cancel", height = 2, width = 20, command = lambda:self.__invested_view_close(True))
         i_cancel.grid(row = 2, column = 0)
-        i_confirm = tk.Button(self.investedView, text = "Confirm", height = 2, width = 20, command = lambda:self.invested_view_close())
+        i_confirm = tk.Button(self.__investedView, text = "Confirm", height = 2, width = 20, command = lambda:self.__invested_view_close())
         i_confirm.grid(row = 2, column = 1)
 
-    def get_invested(self):
+    def __get_invested(self):
         self.__cursor.execute("SELECT invested FROM invested_table")
         return int(self.__cursor.fetchone()[0])
     
-    def invested_view_close(self, cancel = False): #Closes record view
+    def __invested_view_close(self, cancel = False): #Closes record view
         if cancel:
-            self.investedView.destroy()
+            self.__investedView.destroy()
         else:
-            value = self.invested_entry.get()
+            value = self.__invested_entry.get()
             #print(values)
             flag = False #Flags if any values are empty
             if "" == value:
@@ -266,13 +266,13 @@ class CryptoWindow:
                 print("Error: Empty value")
             else:
                 if value[0] == "+" or value[0] == "-":
-                    value = self.get_invested() + int(str(value[0:]))
+                    value = self.__get_invested() + int(str(value[0:]))
                 sql = """UPDATE invested_table SET invested = '"""+str(value)+"""'"""
                 #print(sql)
                 self.__cursor.execute(sql)
                 self.__db.commit()
                 
-                self.investedView.destroy()
+                self.__investedView.destroy()
                 
 def round_(value, to = .5):
     if (value % to) >= (to / 2):
